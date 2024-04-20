6d988d73d93c1d716f1393c8307bedf77212c837
shahanxali <shahanali252002@gmail.com>
2024-04-21 00:46:02 +0530

    Initiate count

---
+++ Array/Sorting/CountingSort.cc
@@ -0,0 +1,6 @@
+//Time Complexity: Worst= n + k, Average= n + k, Best= n + k    (k is the range of the key value that is if array have 0 to 100 numbers then k is 100)
+//Space Complexity: n + k
+
+// https://www.geeksforgeeks.org/counting-sort/
+
+
